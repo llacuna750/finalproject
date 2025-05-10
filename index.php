5702ae8156e541dd7d4bebf70aaff6481c9100dd
llacuna750 <llacuna750@gmail.com>
2025-05-10 12:47:27 +0800

    Add incogfeature

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php 
     $gabfeature = "added gab newfeature";
     $yourfeature = "added your new feature here";
+    $incogfeature = "added incognito feature";
 
     echo "Welcome to PHP!";
     echo "<br> {$gabfeature}";
     echo "<br> {$yourfeature}";
+    echo "<br> {$incogfeature}";
